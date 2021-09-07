4fb4b63fe49e6be0cd5ccd1a4b445fcae0e0cc3a
NgLam is SIMP <33188123+NgLamVN@users.noreply.github.com>
2021-09-07 21:45:04 +0700

    Holy F...

--- src/LamPocketVN/PlayerAuto/features/AutoFix.php
+++ src/LamPocketVN/PlayerAuto/features/AutoFix.php
@@ -24,7 +24,7 @@ class AutoFix implements Listener
     /**
      * @param BlockBreakEvent $event
 	 * @priority HIGHEST
-	 * 2@handleCancelled FALSE
+	 * @handleCancelled FALSE
      */
     public function onBreak(BlockBreakEvent $event)
     {
